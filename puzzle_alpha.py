1ecd9ebcc08b3959ee6caa2f9dbcf2388f136656
MilanRudez <51215680+MilanRudez@users.noreply.github.com>
2019-08-19 09:16:19 +0200

    Create puzzle_alpha.py
    The first iteration of the script in order to automatize all the calls to the base URL + next response

---
+++ puzzle_alpha.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+#The URL to be used the base
+baseUrl = "http://fasttrack.herokuapp.com/"
+
+#The main function to make the calls to the x amount of pages
+def fetch(link):
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    r = requests.get(link, headers=headers)
+    jsonResponse = (json.loads(r.content.decode("UTF-8")))
+    #For debugging purposes
+    print(link)
+    print (jsonResponse)
+    print (headers)
+    fetch(baseUrl + jsonResponse['next'])
+
+fetch(baseUrl)
